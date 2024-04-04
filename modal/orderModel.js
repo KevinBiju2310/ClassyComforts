7987b3f5bf024233e45a39c3226c494a82274f12
Kevin <kevinjamesbiju2000@gmail.com>
2024-04-05 00:16:59 +0530

    added orderModel

--- modal/orderModel.js
+++ modal/orderModel.js
@@ -1,5 +1,76 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const orderSchema = new mongoose.Schema({
-    
-})
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+        required: true,
+    },
+    products: [{
+        productId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 1
+        }
+    }],
+    address: [{
+        name: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: Number,
+            required: true
+        },
+        address: {
+            type: String,
+            required: true
+        },
+        district: {
+            type: String,
+            required: true
+        },
+        state: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        pincode: {
+            type: Number,
+            required: true
+        },
+        addressType: {
+            type: String,
+            enum: ['home', 'work'],
+            required: true
+        }
+    }],
+    paymentMethod: {
+        type: String,
+        enum: ['cod', 'razorpay', 'wallet'],
+        required: true
+    },
+    orderStatus: {
+        type: String,
+        enum: ['pending', 'shipped', 'delivered', 'cancelled', 'returned'],
+        default: 'pending',
+        required: true
+    },
+},
+    {
+        timestamps: true
+    })
+
+
+
+const Order = mongoose.model('order', orderSchema)
+module.exports = Order;
